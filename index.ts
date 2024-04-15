d5c9877276fc96c2bb3123ed9330e778c743efa4
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:46:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Q97
+// this function that return current date in the formate
+function currentDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth()+1;
+    var year = date.getFullYear();
+    return day+"/"+month+"/"+year;
+}
+console.log(currentDate());
